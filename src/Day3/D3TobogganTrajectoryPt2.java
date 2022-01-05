216bb67fff18aa56825f567663464969d78f9ad4
Iva Petkova <iva1petkova@gmail.com>
2022-01-05 14:47:53 +0200

    day 3 pt2

--- src/Day3/D3TobogganTrajectoryPt2.java
+++ src/Day3/D3TobogganTrajectoryPt2.java
@@ -6,29 +6,36 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class D3TobogganTrajectoryPt2 {
-    private static final String PATH = "src/Day3/input_day_3_ex.txt";
+    private static final String PATH = "src/Day3/input_day_3.txt";
 
     public static void main(String[] args) throws IOException {
-        char[][] map = extendMap();
-        System.out.println(countTrees(map));
+
+        long rOneDOne = countTrees(1, 1);
+        long   rThreeDOne = countTrees(3, 1);
+        long  rFiveDOne = countTrees(5, 1);
+        long  rSevenDOne = countTrees(7, 1);
+        long  rOneDTwo = countTrees(1, 2);
+        System.out.println(rOneDOne * rThreeDOne * rFiveDOne * rSevenDOne * rOneDTwo);
     }
 
-    private static int countTrees(char[][] map) {
+
+    private static int countTrees(int deltaRight, int deltaDown) throws IOException {
+        char[][] map = extendMap(deltaRight);
         int trees = 0;
-        int col = 3;
-        for (int row = 1; row < map.length; row++) {
+        int col = deltaRight;
+        for (int row = deltaDown; row < map.length; row += deltaDown) {
             if (map[row][col] == '#') {
                 trees++;
             }
-            col += 3;
+            col += deltaRight;
         }
         return trees;
     }
 
-    private static char[][] extendMap() throws IOException {
+    private static char[][] extendMap(int deltaRight) throws IOException {
         List<String> lines = readInput();
         int rows = lines.size();
-        int cols = (rows - 1) * 3 + 1;
+        int cols = (rows - 1) * deltaRight + 1;
         char[][] map = new char[rows][cols];
         for (int i = 0; i < lines.size(); i++) {
             StringBuilder sb = new StringBuilder(lines.get(i));
